1e626ad8e6ee2bcdee19a35ee549ea4be301039a
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-04-21 19:08:43 +0530

    Implement enquiry form with validation and update styling for Angular Material components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,77 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 // import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatRadioModule } from '@angular/material/radio';
 
 @Component({
   selector: 'app-root',
   imports: [
     // RouterOutlet,
-    MatSlideToggleModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatSnackBarModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
+    MatSlideToggleModule,
     MatCheckboxModule,
     MatButtonToggleModule,
+    MatButtonModule,
     MatRadioModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatCardModule,
+    MatChipsModule,
+    MatProgressBarModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   private _snackBar = inject(MatSnackBar);
+
+  enquiryForm!: FormGroup;
+  enquiryTypes: string[] = [
+    'General',
+    'Product Inquiry',
+    'Support',
+    'Feedback',
+  ];
+
+  longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
+
+  constructor(private fb: FormBuilder) {
+    this.enquiryForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      enquiryType: ['', Validators.required],
+      message: [''],
+    });
+  }
+
+  onSubmit() {
+    if (this.enquiryForm.valid) {
+      console.log('Form Submitted:', this.enquiryForm.value);
+      // TODO: Send to backend API
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
   openSnackBar() {
     this._snackBar.open('Message successfully sent!', 'Dismiss', {
